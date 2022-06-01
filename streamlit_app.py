22dbaf77ce2461dcfecb5aacd97eaaf4e839726a
mrrmattmcclintock <106666850+mrrmattmcclintock@users.noreply.github.com>
2022-06-01 06:20:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinich, and Rocket Smoothie")
+streamlit.text("Hard-Boiled Free Range Egg")
